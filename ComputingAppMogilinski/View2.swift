17b80ab1140609457a46dcdd8c18f05295b91892
Aaron <ajmogilinski2009@gmail.com>
2026-09-08 09:46:22 -0500

    Commit 2, absolutely nothing

---
+++ ComputingAppMogilinski/View2.swift
@@ -0,0 +1,18 @@
+//
+//  View2.swift
+//  ComputingAppMogilinski
+//
+//  Created by AARON MOGILINSKI on 9/8/26.
+//
+
+import SwiftUI
+
+struct View2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View2()
+}
