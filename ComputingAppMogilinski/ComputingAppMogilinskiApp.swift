8a6c4bd353be06c840a6ae9ef456ad70355c40c6
Aaron <ajmogilinski2009@gmail.com>
2026-09-08 09:17:13 -0500

    Initial Commit

---
+++ ComputingAppMogilinski/ComputingAppMogilinskiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputingAppMogilinskiApp.swift
+//  ComputingAppMogilinski
+//
+//  Created by AARON MOGILINSKI on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ComputingAppMogilinskiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
