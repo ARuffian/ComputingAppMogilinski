fd9e4040303b89c6d91560b035debc24219a2d51
ARuffian <ajmogilinski2009@gmail.com>
2026-09-09 20:56:25 -0500

    Submission push

---
+++ ComputingAppMogilinski/View3.swift
@@ -0,0 +1,18 @@
+//
+//  View3.swift
+//  ComputingAppMogilinski
+//
+//  Created by AARON MOGILINSKI on 9/9/26.
+//
+
+import SwiftUI
+
+struct View3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View3()
+}
